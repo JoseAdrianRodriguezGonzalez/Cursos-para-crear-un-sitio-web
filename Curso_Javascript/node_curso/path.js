ae23172a72b4ec30eb08b1cf4bad556c197605a8
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2024-01-13 10:29:15 -0600

    Modificado

--- Curso_Javascript/node_curso/path.js
+++ Curso_Javascript/node_curso/path.js
@@ -2,3 +2,5 @@
 const path=require('path')
 const filepath=path.join('main.css')//une un directorio y lo convierte linux
 console.log(path.basename(filepath))//devuelve el archivo base
+console.log(path.dirname(filepath))//Indica la direccion sin el archivo
+console.log(path.parse(filepath))//lo convierte a objeto
